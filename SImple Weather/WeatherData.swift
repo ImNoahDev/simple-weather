ef61b8b6aeb74468688123988593ed11eda5a4ba
Noah P <noah.pa@yahoo.com>
2024-06-22 12:05:42 +0100

    Setup project, basic weather

---
+++ SImple Weather/WeatherData.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherData.swift
+//  SImple Weather
+//
+//  Created by Noah Patel on 22/06/2024.
+//
+
+import Foundation
---
+++ SImple Weather/WeatherService.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherService.swift
+//  SImple Weather
+//
+//  Created by Noah Patel on 22/06/2024.
+//
+
+import Foundation
