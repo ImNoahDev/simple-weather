a1a47ec6a660cc0c36d68d97f3520b9386f56cec
Noah P <noah.pa@yahoo.com>
2024-06-22 13:11:46 +0100

    add location detection

--- SImple Weather/ContentView.swift
+++ SImple Weather/ContentView.swift
@@ -1,57 +1,76 @@
-// ContentView.swift
-
 import SwiftUI
+import CoreLocation
 
 struct ContentView: View {
-    @State private var city = ""
-    @State private var weatherData: WeatherData?
+    @StateObject private var weatherService = WeatherService()
+    @State private var searchText = ""
     @State private var showingAlert = false
     @State private var alertMessage = ""
-
-    private let weatherService = WeatherService()
-
+    
     var body: some View {
-        VStack {
-            TextField("Enter city name", text: $city, onCommit: fetchWeather)
-                .padding()
+        NavigationView {
+            VStack {
+                TextField("Enter city name", text: $searchText, onCommit: {
+                    weatherService.fetchWeather(for: searchText)
+                    hideKeyboard()
+                })
                 .textFieldStyle(RoundedBorderTextFieldStyle())
-
-            if let weatherData = weatherData {
-                Text("City: \(weatherData.name)")
-                    .font(.title)
-
-                Text("Temperature: \(weatherData.main.temp, specifier: "%.1f") °C")
-                Text("Humidity: \(weatherData.main.humidity, specifier: "%.1f") %")
-
-                Text("Weather: \(weatherData.weather.first?.description ?? "")")
-                    .padding()
+                .padding(.horizontal)
+                
+                if let weatherData = weatherService.weatherData {
+                    WeatherView(weatherData: weatherData)
+                } else {
+                    ProgressView("Fetching Weather...")
+                        .padding()
+                }
+                
+                Spacer()
             }
-
-            Spacer()
+            .navigationBarTitle("Weather")
+            .alert(isPresented: $showingAlert) {
+                Alert(title: Text("Error"), message: Text(alertMessage), dismissButton: .default(Text("OK")))
+            }
         }
-        .padding()
-        .alert(isPresented: $showingAlert) {
-            Alert(title: Text("Error"), message: Text(alertMessage), dismissButton: .default(Text("OK")))
+        .onAppear {
+            weatherService.startLocationUpdates()
+        }
+        .onDisappear {
+            weatherService.stopLocationUpdates()
         }
     }
-
-    private func fetchWeather() {
-        weatherService.fetchWeather(city: city) { result in
-            DispatchQueue.main.async {
-                switch result {
-                case .success(let weatherData):
-                    self.weatherData = weatherData
-                case .failure(let error):
-                    self.alertMessage = error.localizedDescription
-                    self.showingAlert = true
-                }
-            }
-        }
+    
+    private func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct WeatherView: View {
+    let weatherData: WeatherData
+    
+    var body: some View {
+        VStack {
+            Text("\(weatherData.name)")
+                .font(.title)
+                .padding()
+            
+            Text("\(weatherData.main.temp, specifier: "%.1f") °C")
+                .font(.title2)
+            
+            Text("Humidity: \(weatherData.main.humidity, specifier: "%.1f") %")
+                .font(.headline)
+            
+            Text("\(weatherData.weather.first?.description ?? "")")
+                .font(.headline)
+                .padding()
+            
+            Spacer()
+        }
+        .padding()
     }
 }
--- SImple Weather/WeatherService.swift
+++ SImple Weather/WeatherService.swift
@@ -1,36 +1,76 @@
-// WeatherService.swift
-
 import Foundation
+import CoreLocation
 
-class WeatherService {
+class WeatherService: NSObject, ObservableObject {
+    @Published var weatherData: WeatherData?
     private let apiKey = ""
     private let baseURL = "https://api.openweathermap.org/data/2.5/weather"
+    private var locationManager: CLLocationManager!
     
-    func fetchWeather(city: String, completion: @escaping (Result<WeatherData, Error>) -> Void) {
-        let urlString = "\(baseURL)?q=\(city)&appid=\(apiKey)&units=metric"
+    override init() {
+        super.init()
         
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+    }
+    
+    func fetchWeather(for city: String) {
+        let urlString = "\(baseURL)?q=\(city)&appid=\(apiKey)&units=metric"
+        fetchWeather(from: urlString)
+    }
+    
+    func fetchWeather(latitude: Double, longitude: Double) {
+        let urlString = "\(baseURL)?lat=\(latitude)&lon=\(longitude)&appid=\(apiKey)&units=metric"
+        fetchWeather(from: urlString)
+    }
+    
+    private func fetchWeather(from urlString: String) {
         guard let url = URL(string: urlString) else {
-            completion(.failure(NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"])))
+            print("Invalid URL")
             return
         }
         
         URLSession.shared.dataTask(with: url) { (data, response, error) in
             if let error = error {
-                completion(.failure(error))
+                print("Error fetching weather data:", error.localizedDescription)
                 return
             }
             
             guard let data = data else {
-                completion(.failure(NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey: "Data not available"])))
+                print("Data not available")
                 return
             }
             
             do {
-                let weatherData = try JSONDecoder().decode(WeatherData.self, from: data)
-                completion(.success(weatherData))
+                let decodedResponse = try JSONDecoder().decode(WeatherData.self, from: data)
+                DispatchQueue.main.async {
+                    self.weatherData = decodedResponse
+                }
             } catch {
-                completion(.failure(error))
+                print("Error decoding weather data:", error.localizedDescription)
             }
         }.resume()
+    }
+    
+    func startLocationUpdates() {
+        locationManager.startUpdatingLocation()
+    }
+    
+    func stopLocationUpdates() {
+        locationManager.stopUpdatingLocation()
+    }
+}
+
+extension WeatherService: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            fetchWeather(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+            stopLocationUpdates()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Location manager error:", error.localizedDescription)
     }
 }
